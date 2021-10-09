9b8f6795033aca004e4103c988d3d9bd4602afe5
杨超凡 <joebe_yang@126.com>
2021-10-09 18:10:53 +0800

    redis存储用户id和对应的io  client的id

--- server/services/io.js
+++ server/services/io.js
@@ -1,13 +1,20 @@
 let tempUserId=''
+const $r = require('../utils/redis')
 module.exports = io => {
   io.on('connection', client => {
-    console.log('链接成功：',client.id)
-    client.on('login',msg=>{
-      tempUserId=msg.user_id
-      console.log('登陆成功:',client.id,msg.user_id)
+    // console.log('链接成功：',client.id)
+    //用户登录，去获取用户的未读消息数
+    client.on('login',async msg=>{
+      tempUserId=msg.user_id;
+      $r.setData('io-client',{[`${msg.user_id}`]:client.id})
+      console.log(await $r.getDataOne('io-client',tempUserId))
+      // console.log('登陆成功:',client.id,msg.user_id)
     })
+    //用户退出系统，去redis里面删除用户对应的房间id
     client.on('disconnect',()=>{
-      console.log(client.id,tempUserId)
+      // console.log(client.id,tempUserId)
+      $r.delOne('io-client',tempUserId)
+      // $r.quit()
     })
   })
 }
---
+++ server/utils/redis.js
@@ -0,0 +1,44 @@
+const Redis = require('redis');
+const Bluebird = require('bluebird');
+
+Bluebird.promisifyAll(Redis.RedisClient.prototype);
+Bluebird.promisifyAll(Redis.Multi.prototype)
+
+
+const $r = Redis.createClient({
+  host:'127.0.0.1',
+  port:6379
+});
+
+$r.on('ready',function(err,data){
+  if(err){
+    console.log('链接报错')
+  }else{
+    console.log('redis连接成功')
+  }
+})
+
+$r.on('error',function(e){
+  console.log('redis出错:'+e)
+})
+
+$r.on('end',function(e){
+  console.log('redis关闭:'+e)
+})
+
+async function setData(key,data){
+  await $r.hmsetAsync(key,data)
+}
+async function getData(key){
+  return await $r.hgetallAsync(key)
+}
+async function getDataOne(key,field){
+  return await $r.hgetAsync(key,field)
+}
+async function delOne(key,field){
+  return await $r.hdelAsync(key,field)
+}
+
+module.exports={
+  setData,getData,getDataOne,delOne
+}
